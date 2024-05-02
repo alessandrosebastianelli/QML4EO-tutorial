919bccdfcc39084bb481a8e52246b66bdd6743ad
alessandrosebastianelli <alessandroseba1995@gmail.com>
2024-05-02 15:32:34 +0200

    to pennylane

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 import numpy as np
-import itertools
-import qiskit
 import os
 
 #----------------------- Quantum Circuit Settings -----------------------
@@ -8,15 +6,6 @@ NUM_QUBITS      = 4
 NUM_SHOTS       = 1 # for timing reasons is set to 1, but in IRL you want this value to be higher https://quantumcomputing.stackexchange.com/questions/9823/what-is-meant-with-shot-in-quantum-computation
 NUM_LAYERS      = 2
 SHIFT           = np.pi/4
-
-def create_QC_OUTPUTS():
-    measurements = list(itertools.product([0, 1], repeat=NUM_QUBITS))
-    return [''.join([str(bit) for bit in measurement]) for measurement in measurements]
-
-QC_OUTPUTS      = create_QC_OUTPUTS()
-NUM_QC_OUTPUTS  = len(QC_OUTPUTS)
-
-SIMULATOR       = qiskit.Aer.get_backend('qasm_simulator')
 
 #----------------------- Dataset Settings -----------------------
 DATASET_ROOT    = 'EuroSAT_2'
